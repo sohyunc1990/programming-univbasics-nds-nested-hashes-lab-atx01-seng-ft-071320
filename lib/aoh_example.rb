fdb275f49bb6d65b8d9ae1a19856b7594ed2f9f4
Sohyun Lee <sohyunc1990@gmail.com>
2020-07-05 03:02:06 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -26,6 +26,6 @@ def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
   
- literal_aoh[1][:name] = "Joelle van Dyne (PGOAT)"
- aoh_update
+aoh[row][key] = new_value
+aoh
 end
